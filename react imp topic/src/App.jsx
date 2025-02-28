9d646a5c93d94bfa824c7c470df23665d391641b
anmol panday <pandayanmol6@gmail.com>
2025-02-28 23:08:19 +0530

    fix hooks

---
+++ react imp topic/src/App.jsx
@@ -0,0 +1,11 @@
+//subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
+
+import { ReactMemo } from "./hooks/Memo/ReactMemo";
+
+export const App = () => {
+  return (
+    <>
+      <ReactMemo />
+    </>
+  );
+};
